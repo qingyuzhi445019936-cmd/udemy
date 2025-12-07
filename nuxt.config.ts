d3b29ce934aecd89d2294699c34ede2544e35d3d
yuzhi <ext-yuzhi@linkse.io>
2025-12-08 01:21:16 +0800

    添加自动化部署脚本

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,15 @@
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
-  css: ['~/assets/css/main.css'],
+  ssr: false,
+  nitro: { preset: 'static' },
   app: {
+    baseURL: '/udemy/', // 对应 GitHub Pages 仓库子路径
     head: {
-      title: '100 Days of Code - The Complete Python Pro Bootcamp',
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      title: '100 Days of Code',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/udemy/favicon.ico' }
       ],
     },
   },
+  modules: ['@nuxtjs/tailwindcss'],
+  css: ['~/assets/css/main.css'],
 })
